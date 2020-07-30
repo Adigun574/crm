288e7e6b2a4b6348fab376f4845c4f60721bd6ad
Adigun Ibrahim <adigunibrahim574@gmail.com>
2020-07-30 08:09:42 -0700

    work in progress

--- src/app/views/sale/pos/pos.component.ts
+++ src/app/views/sale/pos/pos.component.ts
@@ -276,7 +276,7 @@ export class PosComponent implements OnInit {
       })
       this.sale.payment = newPaymentArray
       // this.sale.payment = this.paymentArray
-      // console.log(this.sale)
+      console.log(this.sale)
       this.saleService.saveSale(this.sale).subscribe(data=>{
         this.savingSaleInvoice = false
         this.savingSale = false
